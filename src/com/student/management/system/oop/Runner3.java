dc7ef3fa3fe829e408cc30f929dc5cc18f38af12
Hanamant <channal.hanamant@gmail.com>
2026-09-07 21:32:42 +0530

    Runner

--- src/com/student/management/system/oop/Runner3.java
+++ src/com/student/management/system/oop/Runner3.java
@@ -6,7 +6,8 @@ public class Runner3 {
 		Student s1 = new RegularStudent.RegularStudentBuilder("Mak", 20, "Pune", "7894561230")
 				.withMarksObtainedInEnglish(89).withMarksObtainedInMaths(78).withMarksObtainedInScience(78)
 				.withRollNumber(21).build();
-System.out.println(s1);
+		System.out.println(s1);
 	}
-
+Student s2= new SportsStudent.SportsStudentBuilder("Kohli", 18, "Delhi", "1234567890").withRollNumber(19).withMarksObtainedInEnglish(78).withMarksObtainedInMaths(78).withMarksObtainedInScience(89).with
+	
 }
