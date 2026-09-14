2519082f99f630c89d556f994dbe0d9fe4ccd4bb
Hanamant <channal.hanamant@gmail.com>
2026-09-14 21:56:38 +0530

    Serialization

--- src/com/student/management/system/oop/Person.java
+++ src/com/student/management/system/oop/Person.java
@@ -1,10 +1,14 @@
 package com.student.management.system.oop;
 
+import java.io.Serializable;
+
 import com.student.management.system.exceptions.InvalidAddressException;
 import com.student.management.system.exceptions.InvalidContactNumberException;
 
-public abstract class Person {
+public abstract class Person implements Serializable{
 
+
+	private static final long serialVersionUID = 1L;
 	protected String name;
 	protected int age;
 	protected String contactNumber;
--- src/com/student/management/system/oop/Runner3.java
+++ src/com/student/management/system/oop/Runner3.java
@@ -6,13 +6,20 @@ public class Runner3 {
 		Student s1 = new RegularStudent.RegularStudentBuilder("Mak", 20, "Pune", "7894561230")
 				.withMarksObtainedInEnglish(89).withMarksObtainedInMaths(78).withMarksObtainedInScience(78)
 				.withRollNumber(21).build();
-		System.out.println(s1);
+		//System.out.println(s1);
 
 		Student s2 = new SportsStudent.SportsStudentBuilder("Raj", 15, "Bombay", "7412589630").withSportsScore(90)
 				.withRollNumber(21).withMarksObtainedInEnglish(89).withMarksObtainedInMaths(89)
 				.withMarksObtainedInScience(89).withMarksObtainedInScience(69).build();
-		System.out.println(s2);
+//		System.out.println(s2);
 
+		Student studentArray[]=new Student[3];
+		studentArray[0]=s1;
+		studentArray[1]=s2;
+		
+		for (Student student : studentArray) {
+			System.out.println(student);
+		}
 	}
 
 }
