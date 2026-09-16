5c9af775b518d5fbb74abb719da94e573b4bf27e
Hanamant <channal.hanamant@gmail.com>
2026-09-16 22:50:42 +0530

    Scanner

---
+++ src/com/collections/ListDemo.java
@@ -0,0 +1,25 @@
+package com.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+		List al=new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.add(23);
+		al.add("hani");
+		al.add(null);
+		
+		System.out.println(al);
+		System.out.println(al.size());
+		  
+		
+		
+		
+		
+	}
+
+}
