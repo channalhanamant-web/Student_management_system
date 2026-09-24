f5134ec65a0a8ba0563ac91d09e6566cd8798221
Hanamant <channal.hanamant@gmail.com>
2026-09-24 12:56:23 +0530

    Set Methods

--- src/com/collections/Set/SetMethodsDemo.java
+++ src/com/collections/Set/SetMethodsDemo.java
@@ -1,10 +1,70 @@
 package com.collections.Set;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
 public class SetMethodsDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		HashSet<String> hashSet=new HashSet<String>();
+		hashSet.add("Jatin");
+		hashSet.add("Uday");
+		hashSet.add("Ritu");
+		hashSet.add("Ajinkya");
+		hashSet.add("Jatin");
+		System.out.println(hashSet);
+		
+		LinkedHashSet<String> linkedHashSet=new LinkedHashSet<String>();
+		linkedHashSet.add("Jatin");
+		linkedHashSet.add("Uday");
+		linkedHashSet.add("Ritu");
+		linkedHashSet.add("Ajinkya");
+		linkedHashSet.add("Jatin");
+		System.out.println(linkedHashSet);
+		
+		TreeSet<String> treeSet=new TreeSet<String>();
+		treeSet.add("Jatin");
+		treeSet.add("Uday");
+		treeSet.add("Ritu");
+		treeSet.add("Ajinkya");
+		treeSet.add("Jatin");
+		System.out.println(treeSet);
+		
+		Iterator<String> data=treeSet.iterator();
+		while (data.hasNext()) {
+			System.out.println(data.next());
+		}
+		TreeSet<String> treeSet1=new TreeSet<String>();
+		System.out.println(treeSet1.isEmpty());
+		
+		System.out.println(treeSet.size());
+		
+		System.out.println(treeSet.contains("Jatin"));
+		
+		System.out.println(treeSet.remove("Jatin"));
+		System.out.println(treeSet);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
